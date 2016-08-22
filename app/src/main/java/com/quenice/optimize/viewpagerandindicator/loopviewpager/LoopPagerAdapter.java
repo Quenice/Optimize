4ee6d235f4a6519d1386b756e49f6a534bf67f86
Quenice <qiubing.it@gmail.com>
2016-08-22 18:57:40 +0800

    Office--> LoopPagerAdapter 解决滑动卡顿问题

--- app/src/main/java/com/quenice/optimize/viewpagerandindicator/loopviewpager/LoopPagerAdapter.java
+++ app/src/main/java/com/quenice/optimize/viewpagerandindicator/loopviewpager/LoopPagerAdapter.java
@@ -2,9 +2,11 @@ package com.quenice.optimize.viewpagerandindicator.loopviewpager;
 
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,9 +41,14 @@ public class LoopPagerAdapter<DATA> extends PagerAdapter implements ViewPager.On
 	@Override
 	public Object instantiateItem(ViewGroup container, int position) {
 		if (views[position] == null) {
-			ImageView imageView = new ImageView(container.getContext());
-			imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
-			imageView.setImageResource(container.getResources().getIdentifier("ic_horizontalview" + mData.get(position), "drawable", container.getContext().getPackageName()));
+//			ImageView imageView = new ImageView(container.getContext());
+//			imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+//			imageView.setImageResource(container.getResources().getIdentifier("ic_horizontalview" + mData.get(position), "drawable", container.getContext().getPackageName()));
+			TextView imageView = new TextView(container.getContext());
+			imageView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+			imageView.setText("" + mData.get(position));
+			imageView.setGravity(Gravity.CENTER);
+			imageView.setTextSize(60);
 			views[position] = imageView;
 		}
 		container.addView(views[position]);
@@ -60,28 +67,25 @@ public class LoopPagerAdapter<DATA> extends PagerAdapter implements ViewPager.On
 
 	@Override
 	public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-//		if (position == 0) {
-//			if (positionOffset - 0.1 <= 0.0f) {
-//				mViewPager.setCurrentItem(mData.size() - 2, false);
-//				Log.e("onPageScrolled", "position=" + position + ", positionOffset=" + positionOffset);
-//			}
-//		} else if (position == mData.size() - 1) {
-//			if (positionOffset - 0.1 <= 0.0f)
-//				mViewPager.setCurrentItem(1, false);
-//			Log.e("onPageScrolled", "position=" + position + ", positionOffset=" + positionOffset);
-//		}
+		//第一个页面，偏移量1到0（从隐藏到显示）
+		if (position == 0) {
+			if (positionOffset - 0.05 <= 0.0f) {
+				mViewPager.setCurrentItem(mData.size() - 2, false);
+				Log.e("onPageScrolled", "position=" + position + ", positionOffset=" + positionOffset);
+			}
+		}
+		//倒数第二个页面（对实际数据来说，是最后一页）偏移量从0到1（从显示到隐藏）
+		else if (position == mData.size() - 2) {
+			if (positionOffset + 0.05 >= 1.0f) {
+				mViewPager.setCurrentItem(1, false);
+				Log.e("onPageScrolled", "position=" + position + ", positionOffset=" + positionOffset);
+			}
+		}
 	}
 
 
 	@Override
 	public void onPageSelected(int position) {
-		if (position == 0) {
-			mViewPager.setCurrentItem(mData.size() - 2, false);
-//			Log.e("onPageSelected", "position=" + position);
-		} else if (position == mData.size() - 1) {
-			mViewPager.setCurrentItem(1, false);
-//			Log.e("onPageSelected", "position=" + position);
-		}
 	}
 
 	@Override
